dc0c468dd121f7ffdbe598eacc2c66e52617396f
Adrian <garciasaaib@gmail.com>
2021-06-01 16:57:20 -0600

    terminando con sass

---
+++ sass/src/app.js
@@ -0,0 +1,9 @@
+import "./styles/ejercicio-sass.scss"
+
+
+// images
+import "./static/avatar.jpg"
+import "./static/galeria1.jpg"
+import "./static/galeria2.jpg"
+import "./static/galeria3.jpg"
+import "./static/galeria4.jpg"
---
+++ sass/webpack/webpack.config.js
@@ -0,0 +1,84 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require("autoprefixer");
+
+module.exports = {
+  entry: "./src/app.js",
+
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "js/bundle.js",
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.pug",
+      minify: {
+        html5: true,
+        collapseWhitespace: true,
+        caseSensitive: true,
+        removeComments: true,
+        removeEmptyElements: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name]-styles.css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: ["pug-loader"],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "static/",
+              useRelativePath: true,
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: "65-90",
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
